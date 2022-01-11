603068f43c36cecab35f3260e3f39c8269d7b3a3
Yevhen Kvasov <yevhen.kvasov@nure.ua>
2022-01-11 16:38:49 +0200

    cross line duplicate fix

--- lib/renderer/chart_painter.dart
+++ lib/renderer/chart_painter.dart
@@ -199,11 +199,6 @@ class ChartPainter extends BaseChartPainter {
           lastPoint, curPoint, lastX, curX, size, canvas);
     }
 
-    if ((isLongPress == true || (isTapShowInfoDialog && isOnTap)) &&
-        isTrendLine == false) {
-      drawCrossLine(canvas, size);
-      drawCrossLineText(canvas, size);
-    }
     if (isTrendLine == true) drawTrendLines(canvas, size);
     canvas.restore();
   }
